899425371f7e1c2c936938d399ae4c06aac433d7
jimmydhanson <88393168+jimmydhanson@users.noreply.github.com>
2021-08-03 08:43:01 -0700

    Add files via upload

---
+++ sliceback.py
@@ -0,0 +1,29 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+challenge1 = letters[16:13:-1]
+print(challenge1)
+
+challenge2 = letters[4::-1]
+print(challenge2)
+
+challenge3 = letters[25:17:-1]
+print(challenge3)
+
+print(letters[:-9:-1])
+
+print(letters[-4:])
+
+print(letters[-1:])
+
+print(letters[:1])
+
+print(letters[0])
+
+backwards1 = letters[25:0:-1]
+print(backwards1)
+
+backwards2 = letters[25::-1]
+print(backwards2)
+
+backwards = letters[::-1]
+print(backwards)
---
+++ strings.py
@@ -0,0 +1,22 @@
+print("today is a good day to learn python")
+print('python is fun')
+print("python's string are easy to use")
+print('We can even include "quotes" in the strings')
+print("hello" + " world")
+greeting = "hello "
+name = input("please enter your name ")
+print(greeting + name)
+
+# if we want a space, we can add that too
+print(greeting + ' ' + name)
+
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+print(name + " is " + age + " years old")
+print(type(age))
